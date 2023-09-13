4e9b00b14ad49774aa5358a86cd6d26b478082ab
Marcos César <marcoscesar.dev@gmail.com>
2023-09-13 00:55:50 -0300

    feat: update database_url

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,14 +6,15 @@ const DB_PASSWORD = 'password';
 const DB_DATABASE = 'postgres';
 
 export const config: DataSourceOptions = {
+  url: process.env.DATABASE_URL,
   type: 'postgres',
   synchronize: true,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   host: DB_HOST,
   port: parseInt(DB_PORT),
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
+  // username: DB_USER,
+  // password: DB_PASSWORD,
+  // database: DB_DATABASE,
   migrations: ['migrations/*.ts'],
   migrationsRun: true,
 };
