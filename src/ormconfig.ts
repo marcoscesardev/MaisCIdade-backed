7d34bc26db8b11786d79a4cf6f8a9c891657564a
Marcos César <marcoscesar.dev@gmail.com>
2023-09-13 01:10:19 -0300

    chore: update credentials

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,20 +1,20 @@
 import { DataSourceOptions } from 'typeorm';
-const DB_HOST = 'localhost';
+const DB_HOST = 'ec2-3-210-173-88.compute-1.amazonaws.com';
 const DB_PORT = '5432';
-const DB_USER = 'octoAplicationUser';
-const DB_PASSWORD = 'password';
-const DB_DATABASE = 'postgres';
+const DB_USER = 'dtjnndbjkseoop';
+const DB_PASSWORD =
+  'a096b3ad8923d946df421dcc1eed571f83f3822bfe153ceb54e9adeea7daff1a';
+const DB_DATABASE = 'd9kfd1k72ioa9n';
 
 export const config: DataSourceOptions = {
-  url: process.env.DATABASE_URL,
   type: 'postgres',
   synchronize: true,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   host: DB_HOST,
   port: parseInt(DB_PORT),
-  // username: DB_USER,
-  // password: DB_PASSWORD,
-  // database: DB_DATABASE,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE,
   migrations: ['migrations/*.ts'],
   migrationsRun: true,
 };
